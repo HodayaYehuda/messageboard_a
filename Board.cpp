8e524dca049898cc19e122232b5b5094df2a1dd9
HodayaYehuda <hodayayeuda@gmail.com>
2021-03-21 19:31:57 +0200

    sec commit

---
+++ Board.cpp
@@ -0,0 +1,9 @@
+
+#include "Board.hpp"
+#include "Direction.hpp"
+using namespace std;
+using namespace ariel;
+void Board::post(int r , int c, Direction d, string str){}
+string Board::read(int r , int c, Direction d, int l){return " ";}
+void Board::string show(){}
+}
