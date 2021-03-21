b548172aa50181aa657e6f0f55eb0199805fa3a2
Hodaya <hodayayeuda@gmail.com>
2021-03-21 20:06:19 +0200

    ready

--- Board.cpp
+++ Board.cpp
@@ -3,7 +3,8 @@
 #include "Direction.hpp"
 using namespace std;
 using namespace ariel;
+
 void Board::post(int r , int c, Direction d, string str){}
 string Board::read(int r , int c, Direction d, int l){return " ";}
-void Board::string show(){}
-}
+void Board::show(){}
+
--- Board.hpp
+++ Board.hpp
@@ -5,10 +5,11 @@ using namespace std;
 
 namespace ariel {
     class Board{
-    public:
-
+ 
     private:
     
+    public:
+   
     void post(int r , int c, Direction d, string str);
     string read(int r , int c, Direction d, int l);
     void show();
